1d36d3d0fc54a16139d30a9cd6c61ac5aaa5bac7
swatijain3103 <79741033+swatijain3103@users.noreply.github.com>
2022-06-19 18:23:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from "./components/Form";
+import './components/App.css';
+
+const App = () => {
+    return(
+        <div>
+            <Form />
+        </div>
+    )
+}
+   
+
+export default App;
